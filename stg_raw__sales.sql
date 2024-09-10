78570d0f3fd0d9c48f50602b1277965b42f34f8c
Ricardo Gawlinski <ricardogawlinskijob@gmail.com>
2024-09-10 16:32:29 +0000

    stg_raw_sales

--- stg_raw__sales.sql
+++ stg_raw__sales.sql
@@ -1,1 +1,22 @@
-select * from {{ sources('raw', 'sales') }}
+ with
+
+ source as (
+
+     select * from {{ source('raw', 'sales') }}
+
+ ),
+
+ renamed as (
+
+     select
+         date_date,
+         orders_id,
+         pdt_id,
+         revenue,
+         quantity
+
+     from source
+
+ )
+
+ select * from renamed
